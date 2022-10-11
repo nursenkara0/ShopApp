5144108b98c79b503854a4841be70f190243fb28
nursenkara0 <nur.senkara@coino.com>
2022-10-11 17:46:20 +0300

    bootstrap klasörü eklendi

---
+++ ShopApp.WebUI/Data/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using ShopApp.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Data
+{
+    public static class ProductRepository
+    {
+        private static List<Product> _products = null;
+        static ProductRepository()
+        {
+            _products = new List<Product> { };
+        }
+
+        //public static List<Product> Products
+        //{
+
+        //}
+    }
+}
--- ShopApp.WebUI/Models/Product.cs
+++ ShopApp.WebUI/Models/Product.cs
@@ -7,9 +7,10 @@ namespace ShopApp.WebUI.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public string ImageUrl { get; set; }
         public string Description { get; set; }
         public bool IsApproved { get; set; }
     }
