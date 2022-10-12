778a9f523878fae33a5b21e0e93b921794c1af95
nursenkara0 <nur.senkara@coino.com>
2022-10-12 17:57:51 +0300

    EfCoreGenericRepository was added.

---
+++ ShopApp.Data/Concrete/EfCore/EfCoreGenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Data.Concrete.EfCore
+{
+    public class EfCoreGenericRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext, new()
+    {
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
